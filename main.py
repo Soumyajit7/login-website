6b6e867e21ffc49a69f572600fb62f0ca88c6908
Soumyajit7 <35652969+Soumyajit7@users.noreply.github.com>
2021-06-23 13:16:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from website import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run()
-#     app.run(debug=True)
+#     app.run()
+    app.run(debug=True)
 #     app.run(debug=False,host='0.0.0.0')
