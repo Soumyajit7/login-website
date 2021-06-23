d93af59224b824a46f961a10ad286f084d3b448f
Soumyajit7 <35652969+Soumyajit7@users.noreply.github.com>
2021-06-23 14:31:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ app = create_app()
 
 if __name__ == '__main__':
 #     app.run()
-    app.run(debug=True)
-#     app.run(debug=False,host='0.0.0.0')
+#     app.run(debug=True)
+    app.run(debug=False,host='0.0.0.0')
